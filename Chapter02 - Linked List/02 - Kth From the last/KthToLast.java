9699f9f7b933b941dadde5ec7c91d9578c9abba8
jai7 <princekrish19@gmail.com>
2020-06-26 20:23:28 +0530

    added additional method for kth last solution

--- Chapter02 - Linked List/02 - Kth From the last/KthToLast.java
+++ Chapter02 - Linked List/02 - Kth From the last/KthToLast.java
@@ -1,9 +1,13 @@
 package com.chapter.linkedlists;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Scanner;
 
 import com.custom.dataStructures.LinkedList;
 import com.custom.dataStructures.LinkedList.Node;
+import com.custom.dataStructures.Tuple2;
 
 public class KthToLast {
 
@@ -16,18 +20,47 @@ public class KthToLast {
 		list.head.next.next.next.next = new Node<Integer>(3);
 		System.out.println("Elements in the list are ");
 		list.printList();
-		int k = 2;
+		Scanner sc = null;
+		int k = 0;
+		int value = -1;
+		try{
+			sc = new Scanner(System.in);
+			System.out.println("Enter the index to be checked from last: ");
+			k = sc.nextInt();
+		} catch(Exception ex){
+			System.out.println("Exception due to " + ex.getMessage());
+		} finally {
+			if(null != sc){
+				sc.close();
+			}
+		}
 		// get kth from the last element data in the linked list
-		int value = getKthLastValue(list.head, k);
+		if(k != 0)
+			value = getKthLastValue(list.head, k);
 		System.out.format("The %d value from the last is %d", k , value);
 		
 	}
 	
 	public static int getKthLastValue(Node head, int posFromEnd){
-		int lenOfList = LinkedList.findListLength(head);
+//		int lenOfList = LinkedList.findListLength(head);
+		Tuple2<Map, Integer> tuple = getMapOfNodesAndLength(head);
+		int lenOfList = tuple.getValue2();
 		int posFromStart = lenOfList - posFromEnd + 1;
-		Optional<Node> node = LinkedList.elementAtPos(head, posFromStart);
-		return node.isPresent() ? (int)node.get().getData(): -1;
+//		Optional<Node> node = LinkedList.elementAtPos(head, posFromStart);
+//		return node.isPresent() ? (int)node.get().getData(): -1;
+		return (posFromStart>0 && posFromStart<lenOfList) ? (int)tuple.getValue1().get(Integer.valueOf(posFromStart)) : -1;
+	}
+	
+	public static Tuple2<Map, Integer> getMapOfNodesAndLength(Node<Integer> head){
+		Node<Integer> temp = head;
+		int index = 1;
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		while(null != temp){
+			map.put(Integer.valueOf(index), temp.getData());
+			++index;
+			temp = temp.next;
+		}
+		return new Tuple2<Map, Integer>(map, index-1);
 	}
 
 }
