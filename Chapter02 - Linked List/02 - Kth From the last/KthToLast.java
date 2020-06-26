aa018baf9cd88b44e585c3375c3370efbe2bf100
jai7 <princekrish19@gmail.com>
2020-06-26 20:39:45 +0530

    added additional solution to get using two pointers

--- Chapter02 - Linked List/02 - Kth From the last/KthToLast.java
+++ Chapter02 - Linked List/02 - Kth From the last/KthToLast.java
@@ -36,19 +36,41 @@ public class KthToLast {
 		}
 		// get kth from the last element data in the linked list
 		if(k != 0)
-			value = getKthLastValue(list.head, k);
+			value = getKthValueUsingTwoPointers(list.head, k);
+		// here if value is -1, then can further handle and throw custom exceptions for element not present or indexoutofrange
 		System.out.format("The %d value from the last is %d", k , value);
 		
 	}
 	
 	public static int getKthLastValue(Node head, int posFromEnd){
-//		int lenOfList = LinkedList.findListLength(head);
+		int lenOfList = LinkedList.findListLength(head);
+		int posFromStart = lenOfList - posFromEnd + 1;
+		Optional<Node> node = LinkedList.elementAtPos(head, posFromStart);
+		return node.isPresent() ? (int)node.get().getData(): -1;
+	}
+	
+	public static int getKthLastValueUsingMap(Node head, int posFromEnd){
 		Tuple2<Map, Integer> tuple = getMapOfNodesAndLength(head);
 		int lenOfList = tuple.getValue2();
 		int posFromStart = lenOfList - posFromEnd + 1;
-//		Optional<Node> node = LinkedList.elementAtPos(head, posFromStart);
-//		return node.isPresent() ? (int)node.get().getData(): -1;
 		return (posFromStart>0 && posFromStart<lenOfList) ? (int)tuple.getValue1().get(Integer.valueOf(posFromStart)) : -1;
+	}
+	
+	public static int getKthValueUsingTwoPointers(Node head, int posFromEnd){
+		Node first = head, second = head;
+		int n= 0;
+		while(n < posFromEnd){
+			if(null == first)
+				return -1;
+			first = first.next;
+			++n;
+		}
+		while(null != first){
+			first = first.next;
+			second = second.next;
+		}
+		
+		return (int) second.getData();
 	}
 	
 	public static Tuple2<Map, Integer> getMapOfNodesAndLength(Node<Integer> head){
